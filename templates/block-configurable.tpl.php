ad384d331c2f0daf7cc1e729b27c9b795669f4c0
Tim Erickson <tim@politalk.org>
2019-07-14 19:23:07 -0500

    Adding div with container class to block-configurable.tpl.php

--- templates/block-configurable.tpl.php
+++ templates/block-configurable.tpl.php
@@ -13,13 +13,15 @@
  */
 ?>
 <div class="<?php print implode(' ', $classes); ?>" <?php print backdrop_attributes($attributes); ?>>
-  <?php print render($title_prefix); ?>
-  <?php if ($title): ?>
-    <h2 class="block-title"><?php print $title; ?></h2>
-  <?php endif; ?>
-  <?php print render($title_suffix); ?>
+  <div class="container">
+    <?php print render($title_prefix); ?>
+    <?php if ($title): ?>
+      <h2 class="block-title"><?php print $title; ?></h2>
+    <?php endif; ?>
+    <?php print render($title_suffix); ?>
 
-  <div class="block-content">
-    <?php print render($content); ?>
+    <div class="block-content">
+      <?php print render($content); ?>
+    </div>
   </div>
 </div>
