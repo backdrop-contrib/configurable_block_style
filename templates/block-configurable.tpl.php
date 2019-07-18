5129dd416c315d8dac7344b4bc03425994b310ac
Tim Erickson <tim@politalk.org>
2019-07-17 19:50:48 -0500

    Issue #1: Providing option to add container class
    Cleaning up code a little. Not sure about best practices in formatting this code.

--- templates/block-configurable.tpl.php
+++ templates/block-configurable.tpl.php
@@ -14,19 +14,18 @@
 ?>
 <div class="<?php print implode(' ', $classes);?>" <?php print backdrop_attributes($attributes);
 ?>>
-<?php if ($content_container):?>
-<div class="container">
-    <p>It worked</p>
-<?php endif;?>
+  <?php if ($content_container):?>
+  <div class="container">
+  <?php endif;?>
     <?php print render($title_prefix);?>
     <?php if ($title):?>
-      <h2 class="block-title"><?php print $title;?></h2>
-<?php endif;?>
+    <h2 class="block-title"><?php print $title;?></h2>
+    <?php endif;?>
     <?php print render($title_suffix);?>
-<div class="block-content">
-<?php print render($content);?>
-</div>
-<?php if ($content_container):?>
-</div>
-<?php endif;?>
+    <div class="block-content">
+      <?php print render($content);?>
+    </div>
+  <?php if ($content_container):?>
+  </div>
+  <?php endif;?>
 </div>
