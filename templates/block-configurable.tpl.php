f256576b0216a5c203a1ac761dc241f52532e049
Tim Erickson (@stpaultim) <tim@stpaultim.com>
2026-03-12 00:18:10 -0500

    Added Dynamic Settings back into Configurable settings

--- templates/block-configurable.tpl.php
+++ templates/block-configurable.tpl.php
@@ -1,31 +1,39 @@
 <?php
 /**
  * @file
- * Template for outputting the default block styling within a Layout.
+ * Template for outputting the configurable block styling within a Layout.
  *
  * Variables available:
+ * - $wrapper_tag: The HTML tag to use for the block wrapper element.
  * - $classes: Array of classes that should be displayed on the block's wrapper.
+ * - $attributes: Attributes (including inline styles) for the block wrapper.
+ * - $content_container: Whether to add a .container div around the content.
  * - $title: The title of the block.
- * - $title_prefix/$title_suffix: A prefix and suffix for the title tag. This
- *   is important to print out as administrative links to edit this block are
- *   printed in these variables.
+ * - $title_prefix/$title_suffix: Prefix and suffix for the title tag (admin links).
+ * - $title_tag: The HTML tag to use for the block title.
+ * - $title_attributes: Attributes (including classes) for the title element.
+ * - $content_tag: The HTML tag to use around the block content.
+ * - $content_attributes: Attributes (including classes) for the content element.
  * - $content: The actual content of the block.
  */
 ?>
-<div class="<?php print implode(' ', $classes);?>" <?php print backdrop_attributes($attributes);
-?>>
+<<?php print $wrapper_tag; ?> class="<?php print implode(' ', $classes); ?>"<?php print backdrop_attributes($attributes); ?>>
   <?php if (!empty($content_container)):?>
   <div class="container">
   <?php endif;?>
     <?php print render($title_prefix);?>
     <?php if ($title):?>
-    <h2 class="block-title"><?php print $title;?></h2>
+    <<?php print $title_tag; ?><?php print backdrop_attributes($title_attributes); ?>><?php print $title; ?></<?php print $title_tag; ?>>
     <?php endif;?>
     <?php print render($title_suffix);?>
-    <div class="block-content">
+    <?php if ($content_tag): ?>
+    <<?php print $content_tag; ?><?php print backdrop_attributes($content_attributes); ?>>
+    <?php endif; ?>
       <?php print render($content);?>
-    </div>
+    <?php if ($content_tag): ?>
+    </<?php print $content_tag; ?>>
+    <?php endif; ?>
   <?php if (!empty($content_container)):?>
   </div>
   <?php endif;?>
-</div>
+</<?php print $wrapper_tag; ?>>
